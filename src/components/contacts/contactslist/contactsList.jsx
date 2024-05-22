c4302cfe5e6350279161fa48bf902f38e1d8521c
abhaypawa <144175829+abhaypawa@users.noreply.github.com>
2024-05-22 19:29:04 +0530

    Update contactsList.jsx

--- src/components/contacts/contactslist/contactsList.jsx
+++ src/components/contacts/contactslist/contactsList.jsx
@@ -3,18 +3,15 @@ import { Link } from "react-router-dom";
 import { ContactService } from "../../../services/Contactservice";
 import Spinner from "../../spinner/spinner";
 
-
-let ContactList = () =>{
-    let[query, setQuery] = useState({
-        text:""
-
-    })
+let ContactList = () => {
+    let [query, setQuery] = useState({ text: "" });
     let [state, setState] = useState({
         loading: false,
-        contacts:[],
-        filteredContacts :[],
-        errorMessage:""
-    })
+        contacts: [],
+        filteredContacts: [],
+        errorMessage: ""
+    });
+
     useEffect(() => {
         const fetchContacts = async () => {
             setState(prevState => ({ ...prevState, loading: true }));
@@ -23,13 +20,14 @@ let ContactList = () =>{
                 setState({
                     loading: false,
                     contacts: response.data,
-                    filteredContacts:response.data,
+                    filteredContacts: response.data,
                     errorMessage: ""
                 });
             } catch (error) {
                 setState({
                     loading: false,
                     contacts: [],
+                    filteredContacts: [],
                     errorMessage: error.message
                 });
             }
@@ -38,43 +36,42 @@ let ContactList = () =>{
         fetchContacts();
     }, []);
 
-    let  clickDelete =async(contactId)=>{
-       
+    let clickDelete = async (contactId) => {
         try {
-            let response = await ContactService.deletContact(contactId)
-            if(response){
+            let response = await ContactService.deleteContact(contactId);
+            if (response) {
                 setState(prevState => ({ ...prevState, loading: true }));
                 let response = await ContactService.getAllContacts();
                 setState({
                     loading: false,
                     contacts: response.data,
-                    filteredContacts:response.data,
+                    filteredContacts: response.data,
                     errorMessage: ""
                 });
-
             }
         } catch (error) {
             setState({
                 loading: false,
                 contacts: [],
+                filteredContacts: [],
                 errorMessage: error.message
             });
         }
-    }   
-// search contacts
-    let searchContacts = (event)=>{
-        setQuery({...query, text:event.target.value})
-        let theContacts = state.contacts.filter(contact=>{
-            return contact.name.toLowerCase().includes(event.target.value.toLowerCase())
-        })
+    };
+
+    let searchContacts = (event) => {
+        setQuery({ ...query, text: event.target.value });
+        let theContacts = state.contacts.filter(contact => {
+            return contact.name && contact.name.toLowerCase().includes(event.target.value.toLowerCase());
+        });
         setState({
             ...state,
-            filteredContacts:theContacts
-        })
-    }
+            filteredContacts: theContacts
+        });
+    };
 
-let {loading, contacts,filteredContacts, errorMessage} = state;
-    return(
+    let { loading, contacts, filteredContacts, errorMessage } = state;
+    return (
         <React.Fragment>
             <section className="contact-search p-3">
                 <div className="container">
@@ -82,110 +79,88 @@ let {loading, contacts,filteredContacts, errorMessage} = state;
                         <div className="row">
                             <div className="col">
                                 <p className="h3 fw-bold">Contact Manager
-                                <Link to={'/contacts/add'} className="btn btn-primary ms-2">
-                                    <i className="fa fa-plus-circle me-2"/>
-                                    Add contacts</Link>
+                                    <Link to={'/contacts/add'} className="btn btn-primary ms-2">
+                                        <i className="fa fa-plus-circle me-2" />
+                                        Add contacts</Link>
                                 </p>
                                 <p className="fst-italic">A contact manager is a tool designed to save, view, add, edit, and delete contact information efficiently. It helps users organize and manage their contacts seamlessly for easy access and updates.</p>
                             </div>
-
                         </div>
                         <div className="row">
                             <div className="col-md-6">
                                 <form className="row">
                                     <div className="col">
-                                    <div className="mb-2">
-                                        <input
-                                        name="text"
-                                        value={query.text}
-                                        onChange={searchContacts}
-                                        type="text" className="form-control" placeholder="Search Names"></input>
-                                    </div>
-
+                                        <div className="mb-2">
+                                            <input
+                                                name="text"
+                                                value={query.text}
+                                                onChange={searchContacts}
+                                                type="text"
+                                                className="form-control"
+                                                placeholder="Search Names"></input>
+                                        </div>
                                     </div>
                                     <div className="col">
-                                    <div className="mb-2">
-                                        <input type="submit" className="btn btn-outline-dark" value="search"></input>
-
+                                        <div className="mb-2">
+                                            <input type="submit" className="btn btn-outline-dark" value="search"></input>
+                                        </div>
                                     </div>
-
-                                    </div> 
                                 </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
-            
-            
-            {
-                loading ? <Spinner/> : <React.Fragment>
 
-                <section className="contact-list">
-                <div className="container">
-                    <div className="row">
-                        {
-                            filteredContacts.length > 0 &&
-                            filteredContacts.map(contacts=>{
-                                return(
-                       
-                                    <div className="col-md-6" key={contacts.id}>
-                                    <div className="card my-2" >
-                                        <div className="card-body">
-                                           <div className="row align-items-center d-flex justify-content-around " >
-                                           <div className="col-md-4">
-                                                <img src= {contacts.imageUrl} className= "contact-img"></img>
-                                                
+            {loading ? <Spinner /> : (
+                <React.Fragment>
+                    <section className="contact-list">
+                        <div className="container">
+                            <div className="row">
+                                {filteredContacts && filteredContacts.length > 0 && filteredContacts.map(contact => (
+                                    <div className="col-md-6" key={contact.id}>
+                                        <div className="card my-2">
+                                            <div className="card-body">
+                                                <div className="row align-items-center d-flex justify-content-around">
+                                                    <div className="col-md-4">
+                                                        <img src={contact.imageUrl} className="contact-img" alt="contact" />
+                                                    </div>
+                                                    <div className="col-md-7">
+                                                        <ul className="list-group">
+                                                            <li className="list-group-item list-group-item-action">
+                                                                Name:<span className="fw-bold">{contact.name}</span>
+                                                            </li>
+                                                            <li className="list-group-item list-group-item-action">
+                                                                Mobile:<span className="fw-bold">{contact.mobile}</span>
+                                                            </li>
+                                                            <li className="list-group-item list-group-item-action">
+                                                                Email:<span className="fw-bold">{contact.email}</span>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                    <div className="col-md-1 d-flex flex-column align-items-center">
+                                                        <Link to={`/contacts/view/${contact.id}`} className="btn btn-warning my-1">
+                                                            <i className="fa fa-eye" />
+                                                        </Link>
+                                                        <Link to={`/contacts/edit/${contact.id}`} className="btn btn-primary my-1">
+                                                            <i className="fa fa-pen" />
+                                                        </Link>
+                                                        <button className="btn btn-danger" onClick={() => clickDelete(contact.id)}>
+                                                            <i className="fa fa-trash" />
+                                                        </button>
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div className="col-md-7">
-                                                <ul className="list-group">
-                                                    <li className="list-group-item list-group-item-action">
-                                                        Name:<span className="fw-bold">{contacts.name}</span>
-                                                        </li>
-                                                        <li className="list-group-item list-group-item-action">
-                                                        Mobile:<span className="fw-bold">{contacts.mobile}</span>
-                                                        </li>
-                                                        <li className="list-group-item list-group-item-action">
-                                                        Email:<span className="fw-bold">{contacts.email}</span>
-                                                        </li>
-                                                </ul>
-                                            </div>
-                                            <div className="col-md-1 d-flex flex-column align-items-center">
-                                                <Link to={`/contacts/view/${contacts.id}`}className="btn btn-warning my-1">
-                                                    <i className="fa fa-eye"/>
-                                                </Link>
-                                                <Link to={`/contacts/edit/${contacts.id}`}className="btn btn-primary my-1">
-                                                    <i className="fa fa-pen"/>
-                                                </Link>
-                                                <button className="btn btn-danger" onClick={()=>clickDelete(contacts.id)}>
-                                                    <i className="fa fa-trash"/>
-                                                </button>
-        
-                                            </div>
-        
-        
-                                           </div>
-        
                                         </div>
                                     </div>
-        
-                                </div>
-                                    )
-                            })
-                        }
-             
-                    </div>
-                </div>
-
-            </section>
-                
+                                ))}
+                            </div>
+                        </div>
+                    </section>
                 </React.Fragment>
-            }
-</React.Fragment>
-            
-            
-        
-    )
+            )}
+        </React.Fragment>
+    );
 };
 
 export default ContactList;
